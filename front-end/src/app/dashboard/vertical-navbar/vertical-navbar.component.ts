4328e0b87db40a173ad79c20589588a5816c7013
nikhilishere30 <nikhilishere30@gmail.com>
2022-02-16 10:18:12 +0530

    Dynamic loading of components

--- front-end/src/app/dashboard/vertical-navbar/vertical-navbar.component.ts
+++ front-end/src/app/dashboard/vertical-navbar/vertical-navbar.component.ts
@@ -8,11 +8,27 @@ import { ComponentModel } from './component.model';
 })
 export class VerticalNavbarComponent implements OnInit {
 
-  components : ComponentModel[] = [];
+  isAdmin : boolean;
+  sizeOfComponents : number;
+  startIndex = 0;
+
+  components : ComponentModel[] = [
+    new ComponentModel('Configuration',"configuration"),
+    new ComponentModel("Dummy","dummy"),
+    new ComponentModel("Item3","item3")
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.isAdmin=true; // To load components dynamically, use UserService and get the Roletype of the user. If its admin
+
+    if(!this.isAdmin)
+    this.startIndex=1;
+
+    this.sizeOfComponents=this.components.length;
+
   }
 
 }
