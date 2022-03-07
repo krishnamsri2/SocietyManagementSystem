4b692bdbd3de2bcea12ff98aaf390ae5aed3a0d0
rajtomarmanitcse <rajtomarmanitcse@gmail.com>
2022-03-07 10:54:58 +0530

    attendance updated

--- back-end/ProjectLatest/src/main/java/com/example/ProjectLatest/Security/ConfigInterceptor.java
+++ back-end/ProjectLatest/src/main/java/com/example/ProjectLatest/Security/ConfigInterceptor.java
@@ -1,19 +1,19 @@
-//package com.example.ProjectLatest.Security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class ConfigInterceptor implements WebMvcConfigurer {
-//    @Bean
-//    public Interceptor getInterceptor() {
-//        return new Interceptor();
-//    }
-//
-//    public @Override void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(getInterceptor()).addPathPatterns("/**");
-//    }
-//
-//}
+package com.example.ProjectLatest.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigInterceptor implements WebMvcConfigurer {
+    @Bean
+    public Interceptor getInterceptor() {
+        return new Interceptor();
+    }
+
+    public @Override void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getInterceptor()).addPathPatterns("/**");
+    }
+
+}
